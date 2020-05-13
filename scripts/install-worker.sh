4e7b8a62cd27cd413bb2cc894e795182cfbf5c20
Amir Hadi <hadi.amir@gmail.com>
2020-05-13 10:59:02 +0200

    Upgrade again to the newest version of the kernel (5.4)

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -48,6 +48,9 @@ fi
 ### Packages ###################################################################
 ################################################################################
 
+# Upgrade to the newest Linux kernel 5.4 (see AWS Case 6992677781 in Production account)
+sudo amazon-linux-extras install -y kernel-ng
+
 # Update the OS to begin with to catch up to the latest packages.
 sudo yum update -y
 
